489d31bea286597c5e7169e1d3e4c533c4055bd9
Xinyi Shen <shenxinyi@Xinyis-MacBook-Pro.local>
2025-02-11 21:57:13 -0800

    add lab 4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,12 +5,54 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # # example of test that checks for logical errors
+    # def test_sulfuras_should_not_decrease_quality(self):
+    #     items = [Item("Sulfuras", 5, 80)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     sulfuras_item = items[0]
+    #     self.assertEqual(80, sulfuras_item.quality)
+    #     self.assertEqual(4, sulfuras_item.sell_in)
+    #     self.assertEqual("Sulfuras", sulfuras_item.name)
+
+    # # example of test that checks for syntax errors
+    # def test_gilded_rose_list_all_items(self):
+    #     items = [Item("Sulfuras", 5, 80)]
+    #     gilded_rose = GildedRose(items)
+    #     all_items = gilded_rose.get_item()
+    #     self.assertEqual(["Sulfuras"], all_items)
+
+    # my tests
+    # 1. The Quality of an item is never more than 50
+    def test_quality_not_more_than_50(self):
+        items = [Item("Aged Brie", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        brie_item = items[0]
+        self.assertEqual(50, brie_item.quality)
 
+    # 2. The Quality of an item is never negative
+    def test_quality_not_negative(self):
+        items = [Item("Sulfuras", 5, -20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        sulfuras_item = items[0]
+        self.assertEqual(0, sulfuras_item.quality)
+
+    # 3. "Conjured" items degrade in Quality twice as fast as normal items
+    def test_conjured_decrease_quality_twice(self):
+        items = [Item(name="Conjured Mana Cake", sell_in=3, quality=6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        conjured_item = items[0]
+        self.assertEqual(4, conjured_item.quality)
+
+    # 4. syntax error
+    def test_nonexistent_attribute(self):
+        items = [Item("Elixir of the Mongoose", 5, 7)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].price)
 
 if __name__ == '__main__':
     unittest.main()
