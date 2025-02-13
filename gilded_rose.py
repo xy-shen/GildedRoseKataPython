6b8b2334c04c4ddc83717bca81bbd833bb3c16a5
Xinyi Shen <shenxinyi@Xinyis-MacBook-Pro.local>
2025-02-12 17:38:08 -0800

    finished lab 5: refactor code and all tests pass

--- gilded_rose.py
+++ gilded_rose.py
@@ -18,8 +18,50 @@ class GildedRose(object):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
+    def get_item(self):
+        ret = []
+        for item in self.items:
+            ret.append(item.name)
+        return ret
+    
+    def get_item_by_name(self, name):
+        ret = []
+        for item in self.items:
+            if item.name == name:
+                ret.append(item)
+        return ret
+
     def update_quality(self):
         for item in self.items:
+            # "Sulfuras" don't need any change
+            if "Sulfuras" not in item.name:
+                if "Aged Brie" in item.name:
+                    item.quality += 1
+                elif "Backstage passes" in item.name:
+                    if item.sell_in < 0:
+                        item.quality = 0
+                    elif item.sell_in < 6:
+                        item.quality += 3
+                    elif item.sell_in < 11:
+                        item.quality += 2
+                    else:
+                        item.quality += 1
+                elif "Conjured" in item.name:
+                    if item.sell_in < 0:
+                        item.quality -= 4
+                    else:
+                        item.quality -= 2
+                else:
+                    if item.sell_in < 0:
+                        item.quality -= 2
+                    else:
+                        item.quality -= 1
+                item.quality = min(item.quality, 50)
+                
+            item.quality = max(item.quality, 0)
+            item.sell_in = item.sell_in - 1
+            
+            """
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -47,3 +89,4 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+            """
